75720393d6daaac023143b282ce205c159920a8b
Andrey Shelikhanov <andrey.shelikhanov@gmail.com>
2023-04-06 00:50:54 +0300

    variables names has been changed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 15465; // Стоимость билета
-        int bonus_mile_per_cost = 20; // Кол-во рублей за которое начисляется 1 миля
-        int bonus_amount; // Кол-во бонусных миль
+        int ticketPrice = 15465; // Стоимость билета
+        int bonusMilePerCost = 20; // Кол-во рублей за которое начисляется 1 миля
+        int bonusAmount; // Кол-во бонусных миль
 
-        bonus_amount = (ticket_price / bonus_mile_per_cost);
+        bonusAmount = (ticketPrice / bonusMilePerCost);
         System.out.println("Стоимость вашего билета составила");
-        System.out.println(ticket_price + " Рубль(ей/ля)");
+        System.out.println(ticketPrice + " Рубль(ей/ля)");
         System.out.println("Количество начисленных бонусных миль");
-        System.out.println(bonus_amount + " Миля(ль/ли)");
+        System.out.println(bonusAmount + " Миля(ль/ли)");
 
     }
 
